0bacb688169a3aa7becd6d98a838306827c92860
Tim Tallberg <tim.tallis@hotmail.com>
2024-10-31 14:20:21 +0100

    Removed pointless code

--- todo_demo/Functions.cs
+++ todo_demo/Functions.cs
@@ -2,25 +2,6 @@
 {
     public class Functions
     {
-        public static List<Models.TodoItem> TodoItems { get; set; } = new List<Models.TodoItem>();
-
-        public static List<Models.TodoItem> CreateList()
-        {
-            TodoItems = new List<Models.TodoItem>();
-
-            return TodoItems;
-        }
-
-        public static List<Models.TodoItem> UpdateList(List<Models.TodoItem> list)
-        {
-            TodoItems.AddRange(list);
-
-            return TodoItems;
-        }
-
-        public static List<Models.TodoItem> GetList()
-        {
-            return TodoItems;
-        }
+        
     }
 }
