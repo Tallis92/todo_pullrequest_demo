db6d78e36f3bf22f889b0bb11ab11e574bfc38f0
Tim Tallberg <tim.tallis@hotmail.com>
2024-11-01 14:34:16 +0100

    Added nullable types

--- todo_demo/Models/TodoItem.cs
+++ todo_demo/Models/TodoItem.cs
@@ -3,8 +3,8 @@
     public class TodoItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public bool IsCompleted { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public bool? IsCompleted { get; set; }
     }
 }
